69ad9a56cd84c479353992298dc6829ce1189015
saheenshoukath <saheenshoukath@gmail.com>
2022-01-21 00:28:44 +0530

    added layout

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,9 @@
+const MainLayout = () => {
+  return (
+    <h1>
+      <p>Main Layout</p>
+    </h1>
+  )
+}
+
+export default MainLayout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next'
+import MainLayout from '@/components/layouts/MainLayout'
 
 const Home: NextPage = () => {
   return (
     <div>
+      <MainLayout />
       <p className='text-red-500 text-2xl'>Hello World</p>
     </div>
   )
