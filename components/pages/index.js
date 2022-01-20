4d59d774cb6b04e85c90b64413ddc992d2ea2f9a
saheenshoukath <saheenshoukath@gmail.com>
2022-01-20 21:43:58 +0530

    dev init

--- components/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as HeroSection } from './HeroSection'
-export { default as AboutSection } from './AboutSection'
-export { default as ProjectSection } from './ProjectSection'
-export { default as ContactSection } from './ContactSection'
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <p className='text-red-500 text-2xl'>Hello World</p>
+    </div>
+  )
+}
+
+export default Home
