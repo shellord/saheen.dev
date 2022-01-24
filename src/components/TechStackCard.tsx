f55e74d14ef6fbfd221e01f8dd44d7b10c04ea99
saheenshoukath <saheenshoukath@gmail.com>
2022-01-24 19:46:50 +0530

    added tech stack light svg logos

--- src/components/TechStackCard.tsx
+++ src/components/TechStackCard.tsx
@@ -19,7 +19,7 @@ const TechStackCard = ({
   return (
     <Link href={href}>
       <a target='_blank'>
-        <div className='bg-gray-500 flex flex-col rounded-lg overflow-hidden shadow-lg hover:scale-110 transition-all duration-300'>
+        <div className='bg-gray-800 flex flex-col rounded-lg overflow-hidden shadow-lg hover:scale-110 transition-all duration-300'>
           <div className='flex align-center justify-center py-7 px-7 flex-1'>
             <div className='relative flex flex-1 h-[3rem]'>
               <Image
