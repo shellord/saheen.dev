e5a3faa1ed53d4de893e063cbee2c246845b67bf
saheenshoukath <saheenshoukath@gmail.com>
2022-05-16 23:14:50 +0530

    add projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -125,36 +125,6 @@ const Home: NextPage = () => {
           </a>
         </Link>
       </div>
-      {/* <div className='mt-16'>
-        <p className='text-2xl sm:text-4xl font-bold'>My Projects</p>
-        <div className='mt-5 grid grid-flow-row gap-10 md:grid-flow-col grid-rows-3'>
-          <ProjectCard
-            title='mash coin'
-            description='mash coin description'
-            image='/images/mash.jpeg'
-          />
-          <ProjectCard
-            title='mash coin'
-            description='mash coin description'
-            image='/images/saheen.jpg'
-          />
-          <ProjectCard
-            title='mash coin'
-            description='mash coin description'
-            image='/images/saheen.jpg'
-          />
-          <ProjectCard
-            title='mash coin'
-            description='mash coin description'
-            image='/images/saheen.jpg'
-          />
-          <ProjectCard
-            title='mash coin'
-            description='mash coin description'
-            image='/images/saheen.jpg'
-          />
-        </div>
-      </div> */}
     </div>
   )
 }
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,35 @@
+import ProjectCard from '@/components/ProjectCard'
 import { NextPage } from 'next'
+import { InferGetStaticPropsType } from 'next'
+import projects from '../../data/projects.json'
 
-const Projects: NextPage = () => {
-  return <p>Projects</p>
+const Projects: NextPage<null> = ({ projects }: getStaticPropsType) => {
+  return (
+    <div className='mt-10'>
+      <p className='text-2xl sm:text-4xl font-bold'>My Projects</p>
+      <div className='mt-5 flex flex-wrap'>
+        {projects.items.map((project) => (
+          <div key={project.id} className='w-full md:w-1/2 sm:pr-4 mb-5 '>
+            <ProjectCard
+              title={project.title}
+              description={project.description}
+              image={project.image}
+              href={project.href}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
+type getStaticPropsType = InferGetStaticPropsType<typeof getStaticProps>
+
+export async function getStaticProps() {
+  return {
+    props: {
+      projects,
+    },
+  }
+}
 export default Projects
